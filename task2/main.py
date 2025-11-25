e9a369fc8652efeb53cfc599779f56f38f50f838
mx-smblt <mx.smblt@gmail.com>
2025-11-25 16:39:52 +0200

    Homework.

--- task2/main.py
+++ task2/main.py
@@ -1,34 +1,41 @@
-import turtle
+import argparse
+from turtle import Screen, Turtle
+
+SIZE = 300
 
 
-def koch_curve(t, order, size):
-    if order == 0:
-        t.forward(size)
+def koch_curve(turtle: Turtle, recursion_depth: int, size: float) -> None:
+    if recursion_depth == 0:
+        turtle.forward(size)
     else:
         for angle in [60, -120, 60, 0]:
-            koch_curve(t, order - 1, size / 3)
-            t.left(angle)
+            koch_curve(turtle, recursion_depth - 1, size / 3)
+            turtle.left(angle)
 
 
-def draw_koch_curve(order, size=300):
-    window = turtle.Screen()
+def draw_koch_curve(recursion_depth: int, size: float) -> None:
+    window = Screen()
     window.bgcolor("white")
 
-    t = turtle.Turtle()
-    t.speed(0)
-    t.penup()
-    t.goto(-size / 2, size / 3)
-    t.pendown()
+    turtle = Turtle()
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(-size / 2, size / 3)
+    turtle.pendown()
 
     for _ in range(3):
-        koch_curve(t, order, size)
-        t.right(120)
+        koch_curve(turtle, recursion_depth, size)
+        turtle.right(120)
 
     window.mainloop()
 
 
 def main():
-    draw_koch_curve(3)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--recursion", "-r", default="3", help="Recursion depth")
+    args = vars(parser.parse_args())
+    recursion_depth = int(args.get("recursion"))
+    draw_koch_curve(recursion_depth, SIZE)
 
 
 if __name__ == "__main__":
